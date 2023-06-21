e99f49f33e6198e0544e1245c16facf3b8f71a21
Ryam AlMalki <86604450+RyamAlmalki@users.noreply.github.com>
2023-06-21 11:14:00 +0300

    Update login_or_register_screen.dart

--- lib/screens/login_or_register_screen.dart
+++ lib/screens/login_or_register_screen.dart
@@ -1,4 +1,5 @@
-import 'package:chatapp/screens/widgets/button.dart';
+import 'package:chatapp/screens/login_screen.dart';
+import 'package:chatapp/screens/widgets/style.dart';
 import 'package:flutter/material.dart';
 
 class LoginOrRegister extends StatefulWidget {
@@ -18,9 +19,34 @@ class _LoginOrRegisterState extends State<LoginOrRegister> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              createButton('Login'),
+                ElevatedButton(
+                style: buttonStyle(),
+                onPressed: (){
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginScreen()),
+                  );
+                }, 
+                child: const Text('Login', style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20, 
+                    fontStyle: FontStyle.normal,
+                  ),
+                ),
+              ),
+
               const SizedBox(height: 10,),
-              createButton('Register'),
+
+              ElevatedButton(
+                style: buttonStyle(),
+                onPressed: (){}, 
+                child: const Text('Register', style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20, 
+                    fontStyle: FontStyle.normal,
+                  ),
+                ),
+              )
             ],
           ),
         )

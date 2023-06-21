0d1021394e5e79b70e50b4e85db0b689130300ac
ryammalki <ryam.malki@gmail.com>
2023-06-21 11:20:09 +0300

    buttonstyle update

--- lib/screens/widgets/button.dart
+++ lib/screens/widgets/button.dart
@@ -1,18 +1,9 @@
 import 'package:flutter/material.dart';
 
-Widget createButton(String? title){
-  return ElevatedButton(
-    style: ElevatedButton.styleFrom(
-        minimumSize: const Size(300 , 50), //////// HERE
-        backgroundColor: Colors.white,
-        side: const BorderSide(color: Colors.indigoAccent, width: 2),
-      ),
-    onPressed: (){}, 
-    child: Text('$title', style: const TextStyle(
-        color: Colors.black,
-        fontSize: 20, 
-        fontStyle: FontStyle.normal,
-      ),
-    ),
+ButtonStyle buttonStyle(){
+  return ElevatedButton.styleFrom(
+    minimumSize: const Size(300 , 50), //////// HERE
+    backgroundColor: Colors.white,
+    side: const BorderSide(color: Colors.indigoAccent, width: 2),
   );
 }

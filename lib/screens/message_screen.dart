3cee5a7a9b65089bdd36d6221a786c5c3ef1db5c
ryammalki <ryam.malki@gmail.com>
2023-07-05 21:38:18 +0300

    update

--- lib/screens/message_screen.dart
+++ lib/screens/message_screen.dart
@@ -1,6 +1,8 @@
+import 'package:chatapp/screens/widgets/searchbar.dart';
+import 'package:chatapp/screens/widgets/user_list.dart';
 import 'package:chatapp/screens/widgets/user_message_tile.dart';
 import 'package:flutter/material.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../const.dart';
 
 class MessageScreen extends StatefulWidget {
@@ -18,8 +20,10 @@ class _MessageScreentState extends State<MessageScreen> {
       body: SafeArea(
       child: Column(
         children: [
-          UserMessageTile(),
-        ],
+            UserMessageTile(),
+            Expanded(child: ListView(scrollDirection: Axis.horizontal,)),
+            SearchBar(), 
+          ],
         )
       ),
     );

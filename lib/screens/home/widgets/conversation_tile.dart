8bf20d02d6e8d0f91617932aab665dbfb3833cb8
ryammalki <ryam.malki@gmail.com>
2023-07-30 22:52:45 +0300

    update

--- lib/screens/home/widgets/conversation_tile.dart
+++ lib/screens/home/widgets/conversation_tile.dart
@@ -16,6 +16,9 @@ class ConversationTile extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(top: 2),
       child: ListTile(
+        shape: UnderlineInputBorder(
+
+         ),
         subtitle: Padding(
           padding: const EdgeInsets.only(top: 5),
           child: Row(
